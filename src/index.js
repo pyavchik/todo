f125d0d886aa04914a622cdcf73ae482e028200b
pyavchik <pyavchik@gmail.com>
2018-10-18 23:59:48 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,23 +1,7 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
-const TodoList = () => {
-    return (
-        <ul>
-            <li>Drink cup of tea</li>
-            <li>Build awesome application</li>
-            <li>Walk with pet</li>
-        </ul>
-    )
-};
+import App from './components/app';
 
-const el = (
-    <div>
-        <h1>My Todo List</h1>
-        <input placeholder="search"/>
-        <TodoList/>
-    </div>
-
-)
-
-ReactDom.render(el, document.getElementById('root'));
+ReactDOM.render(<App />,
+  document.getElementById('root'));
