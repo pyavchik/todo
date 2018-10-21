f50fb223ac9fd667bece0b4e3e73daf8fc02e2e6
pyavchik <pyavchik@gmail.com>
2018-10-21 21:47:34 +0300

    added searching added filtering

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,117 +1,138 @@
 import React, { Component } from 'react';
 
 import AppHeader from '../app-header';
-import SearchPanel from '../search-panel';
 import TodoList from '../todo-list';
+import SearchPanel from '../search-panel';
 import ItemStatusFilter from '../item-status-filter';
 import ItemAddForm from '../item-add-form';
 
 import './app.css';
 
-export default class App extends Component{
-    maxId = 100;
 
-    state = {
-        todoData: [
-            this.createTodoItem('Drink Coffee'),
-            this.createTodoItem('Make Awesome App'),
-            this.createTodoItem('Have a lunch')
-        ]
+export default class App extends Component {
+
+  maxId = 100;
+
+  state = {
+    items: [
+      { id: 1, label: 'Drink Coffee', important: false, done: false },
+      { id: 2, label: 'Learn React', important: true, done: false },
+      { id: 3, label: 'Make Awesome App', important: false, done: false }
+    ],
+    filter: 'all',
+    search: ''
+  };
+
+  onItemAdded = (label) => {
+    this.setState((state) => {
+      const item = this.createItem(label);
+      return { items: [...state.items, item] };
+    })
+  };
+
+  toggleProperty = (arr, id, propName) => {
+    const idx = arr.findIndex((item) => item.id === id);
+    const oldItem = arr[idx];
+    const value = !oldItem[propName];
+
+    const item = { ...arr[idx], [propName]: value } ;
+    return [
+      ...arr.slice(0, idx),
+      item,
+      ...arr.slice(idx + 1)
+    ];
+  };
+
+  onToggleDone = (id) => {
+    this.setState((state) => {
+      const items = this.toggleProperty(state.items, id, 'done');
+      return { items };
+    });
+  };
+
+  onToggleImportant = (id) => {
+    this.setState((state) => {
+      const items = this.toggleProperty(state.items, id, 'important');
+      return { items };
+    });
+  };
+
+  onDelete = (id) => {
+    this.setState((state) => {
+      const idx = state.items.findIndex((item) => item.id === id);
+      const items = [
+        ...state.items.slice(0, idx),
+        ...state.items.slice(idx + 1)
+      ];
+      return { items };
+    });
+  };
+
+  onFilterChange = (filter) => {
+    this.setState({ filter });
+  };
+
+  onSearchChange = (search) => {
+    this.setState({ search });
+  };
+
+  createItem(label) {
+    return {
+      id: ++this.maxId,
+      label,
+      important: false,
+      done: false
     };
+  }
 
-    createTodoItem(label) {
-        return {
-            label,
-            important: false,
-            done: false,
-            id: this.maxId++
-        }
+  filterItems(items, filter) {
+    if (filter === 'all') {
+      return items;
+    } else if (filter === 'active') {
+      return items.filter((item) => (!item.done));
+    } else if (filter === 'done') {
+      return items.filter((item) => item.done);
+    }
+  }
+
+  searchItems(items, search) {
+    if (search.length === 0) {
+      return items;
     }
 
-    deleteItem = (idx) => {
-        this.setState(({todoData}) => {
+    return items.filter((item) => {
+      return item.label.toLowerCase().indexOf(search.toLowerCase()) > -1;
+    });
+  }
 
-            const newArray = [
-                ...todoData.slice(0, idx),
-                ...todoData.slice(idx + 1)
-            ];
+  render() {
+    const { items, filter, search } = this.state;
+    const doneCount = items.filter((item) => item.done).length;
+    const toDoCount = items.length - doneCount;
+    const visibleItems = this.searchItems(this.filterItems(items, filter), search);
 
-            return {
-                todoData: newArray
-            };
-        });
-    };
+    return (
+      <div className="todo-app">
+        <AppHeader toDo={toDoCount} done={doneCount}/>
 
-    addItem = (text) => {
-        const newItem = this.createTodoItem(text);
+        <div className="search-panel d-flex">
+          <SearchPanel
+            onSearchChange={this.onSearchChange}/>
 
-        this.setState(({todoData}) => {
-            const newArr = [
-                ...todoData,
-                newItem
-            ];
-            return {
-                todoData: newArr
-            };
-        });
+          <ItemStatusFilter
+            filter={filter}
+            onFilterChange={this.onFilterChange} />
+        </div>
 
-        console.log('Added', text);
-    };
+        <TodoList
+          items={ visibleItems }
+          onToggleImportant={this.onToggleImportant}
+          onToggleDone={this.onToggleDone}
+          onDelete={this.onDelete} />
 
-    toggleProperty(arr, id, propName) {
-        const idx = arr.findIndex((el) => el.id === id);
-
-        const oldItem = arr[idx];
-        const newItem = {...oldItem,
-            [propName]: !propName[propName]};
-
-        return [
-            ...arr.slice(0, idx),
-            newItem,
-            ...arr.slice(idx + 1)
-        ];
-    }
-
-    onToggleDone = (id) => {
-        this.setState(({ todoData }) => {
-            return {
-                todoData: this.toggleProperty(todoData, id, 'done')
-            };
-        });
-    };
-
-    onToggleImportant = (id) => {
-        this.setState(({ todoData }) => {
-            return {
-                todoData: this.toggleProperty(todoData, id, 'important')
-            };
-        });
-    };
-
-    render() {
-        const { todoData } = this.state;
-        const doneCount = todoData
-                    .filter((el) => el.done).length;
-
-        const todoCount = todoData.length - doneCount;
-        return (
-            <div className="todo-app">
-                <AppHeader toDo={todoCount} done={doneCount} />
-                <div className="top-panel d-flex">
-                    <SearchPanel />
-                    <ItemStatusFilter />
-                </div>
-
-                <TodoList
-                    todos={todoData}
-                    onDeleted={ this.deleteItem }
-                    onToggleImportant={this.onToggleImportant}
-                    onToggleDone={this.onToggleDone}
-                />
-                <ItemAddForm onItemAdded={this.addItem}/>
-            </div>
-        );
-    }
-};
-
-
+        <ItemAddForm
+          onItemAdded={this.onItemAdded} />
+      </div>
+    );
+  };
+}
--- src/components/item-add-form/item-add-form.js
+++ src/components/item-add-form/item-add-form.js
@@ -1,40 +1,42 @@
-import React, {Component} from 'react';
-import './item-add-form.css'
+import React, { Component } from 'react';
+
+import './item-add-form.css';
 
 export default class ItemAddForm extends Component {
-    state = {
-        label: ''
-    };
 
-    onLabelChange = (e) => {
-        this.setState({
-            label: e.target.value
-        });
-    };
+  state = {
+    label: ''
+  };
 
-    onSubmit = (e) => {
-        this.props.onItemAdded(this.state.label);
-        this.setState({
-            label: ''
-        });
+  onLabelChange = (e) => {
+    this.setState({
+      label: e.target.value
+    })
+  };
 
-        e.preventDefault();
-    };
+  onSubmit = (e) => {
+    e.preventDefault();
+    const { label } = this.state;
+    this.setState({ label: '' });
+    const cb = this.props.onItemAdded || (() => {});
+    cb(label);
+  };
 
-    render(){
-        return (
-            <form className="item-add-form d-flex"
-                    onSubmit={this.onSubmit}>
-                <input type="text"
-                    className="form-control"
-                    onChange={this.onLabelChange}
-                    placeholder="What needs to do"
-                    value={this.state.label}/>
-                <button
-                    className="btn btn-outline-secondary" >
-                    Add Item
-                </button>
-            </form>
-        )
-    }
+  render() {
+    return (
+      <form
+        className="bottom-panel d-flex"
+        onSubmit={this.onSubmit}>
+
+        <input type="text"
+               className="form-control new-todo-label"
+               value={this.state.label}
+               onChange={this.onLabelChange}
+               placeholder="What needs to be done?" />
+
+        <button type="submit"
+                className="btn btn-outline-secondary">Add</button>
+      </form>
+    );
+  }
 }
